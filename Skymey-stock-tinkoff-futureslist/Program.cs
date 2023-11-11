de4df943866ac4aec47b98359718b13ab1cc15ed
dotnetcsharp0x <dsotnikov1996@gmail.com>
2023-11-11 10:14:32 +0300

    Добавьте файлы проекта.

---
+++ Skymey-stock-tinkoff-futureslist/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Skymey_stock_tinkoff_futureslist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
